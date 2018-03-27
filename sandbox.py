9fa085245e335de36583bc56510bb5c8b96b6832
Victor Shan <lninjavs@gmail.com>
2018-03-27 14:35:30 -0700

    Played more with sandbox equation solving, tested powers, not working is solving multiplication with multiple variables.

--- sandbox.py
+++ sandbox.py
@@ -1,10 +1,19 @@
 from sympy import sympify, solve, Symbol, Eq
 
-rhs = sympify(input("RHS: "))
-lhs = sympify(input("LHS: "))
+
+def equation_input():
+    input_equation = input("input equation:")
+    equation_list = input_equation.split("=")
+    global rhs, lhs
+    rhs = sympify(equation_list[1].strip())
+    lhs = sympify(equation_list[0].strip())
+
+
+equation_input()
 equation = Eq(lhs, rhs)
 print(equation)
+print((equation.free_symbols()))
 # eq_x = solve(rhs-lhs, Symbol("x"))
-eq_x = solve(equation, Symbol("x"))
+eq_x = solve(equation, Symbol("mx"))
 print(type(eq_x))
 print(eq_x)
